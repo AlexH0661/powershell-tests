16582cb42f8507cb47f56c71225909d97005d38d
alexh0661 <alexander.hussey@outlook.com>
2021-07-07 13:49:10 +1000

    Add UDP stream test with video

--- vlc-screen-share.ps1
+++ vlc-screen-share.ps1
@@ -2,12 +2,17 @@
 # Screen capture script
 #
 
+# Arguments
+param($dest=":8443/stream", $duration=120)
+write-host("Listening at: {0}" -f $dest)
+write-host("For duration: {0}" -f $duration)
+
 # Checkin to C2
 & "$PSScriptRoot\init.ps1" -stage screen-capture -state starting
 
 # Start stream headless using HTTP. Variables will need to be tuned for performance
 $duration = 120
-& "C:\Program Files (x86)\VideoLAN\VLC\vlc.exe" --% --intf dummy screen://  :screen-fps=30.000000 :live-caching=100 :sout=#transcode{vcodec=h264,acodec=mpga,ab=128,channels=2,samplerate=44100,scodec=none}:http{mux=ffmpeg{mux=flv},dst=:8080/wds} :no-sout-all :sout-keep
+& "C:\Program Files (x86)\VideoLAN\VLC\vlc.exe" "--%" "--intf dummy screen://  :screen-fps=30.000000 :live-caching=100 :sout=#transcode{vcodec=h264,acodec=mpga,ab=128,channels=2,samplerate=44100,scodec=none}:http{mux=ffmpeg{mux=flv},dst=$dest} :no-sout-all :sout-keep"
 $childProcess = Get-Process vlc
 if ($childProcess)
 {
---
+++ vlc-udp-stream.ps1
@@ -0,0 +1,34 @@
+﻿#
+# UDP stream script
+#
+
+# Define arguments
+param($dest="127.0.0.1:1234", $file="C:\Users\user\Downloads\Rick%20Astley%20-%20Never%20Gonna%20Give%20You%20Up.mp4", $duration=120)
+write-host("Targeting: {0}" -f $dest)
+write-host("Using file: {0}" -f $file)
+write-host("For duration: {0}" -f $duration)
+
+# Checkin to C2
+& "$PSScriptRoot\init.ps1" -stage udp-stream -state starting
+
+# Start stream headless using UDP. Variables will need to be tuned for performance
+& "C:\Program Files (x86)\VideoLAN\VLC\vlc.exe" "--%" "--intf dummy file:///$file :file-caching=100 :sout=#transcode{vcodec=h264,acodec=mpga,ab=128,channels=2,samplerate=44100,scodec=none}:udp{dst=$dest} :no-sout-all :sout-keep"
+$childProcess = Get-Process vlc
+if ($childProcess)
+{
+    & "$PSScriptRoot\init.ps1" -stage udp-stream -state started -duration "$duration seconds"
+    write-host("VLC PID: {0}" -f $childProcess.Id)
+    write-host("Sleeping for $duration seconds")
+    start-sleep -s $duration
+    $stopProcess = Stop-Process -Id $childProcess.Id
+    if ($stopProcess -eq $null)
+    {
+        & "$PSScriptRoot\init.ps1" -stage udp-stream -state complete
+    } else
+    {
+        write-host($stopProcess)
+    }
+} else
+{
+    & "$PSScriptRoot\init.ps1" -stage udp-stream -state failed
+}
