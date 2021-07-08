e42fde530a8f5ef7bc2f1728eca2253ef6313806
alexh0661 <alexander.hussey@outlook.com>
2021-07-08 17:42:49 +1000

    invoke-command testing

---
+++ ic-test.ps1
@@ -0,0 +1,5 @@
+﻿$server = "172.25.0.139"
+$cred = Get-Credential
+
+$scriptblock = { & "C:\Users\user\Desktop\scripts\vlc-screen-share.ps1" }
+Invoke-Command -ComputerName $server -Credential $cred -ScriptBlock $scriptblock
